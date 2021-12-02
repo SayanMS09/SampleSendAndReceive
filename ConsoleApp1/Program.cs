445f7c4063e611ac65386baae0f7bfbddb088dc8
Sayan Ghosh <sayanghosh@microsoft.com>
2021-12-02 10:31:12 +0530

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            Console.WriteLine(ip.ToString());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
